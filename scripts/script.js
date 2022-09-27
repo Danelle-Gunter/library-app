7c2fc4477b361581047c303dd866bbcdb8dc6701
Danelle <rhowia@gmail.com>
2022-09-27 17:46:12 -0400

    Add and remove functionality works

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,81 @@
-let myLibrary = [
-    // {
-    //     "title":"Jane Eyre",
-    //     "author":"Charlotte Bronte"
-    // },
-    // {
-    //     "title":"Pride and Prejudice",
-    //     "author":"Jane Austen"
-    // },
-    // {
-    //     "title":"Princess of Mars",
-    //     "author":"Edgar Rice Burroughs"
-    // }
-];
+let myLibrary = [];
 
 function renderBookList(book) {
     const list = document.querySelector('#book-display');
+    const item = document.querySelector(`[data-key='${book.id}']`);
+
+    if (book.deleted) {
+        item.remove();
+        return;
+    }
+
+    list.addEventListener('click', event => {
+        if (event.target.classList.contains('del-btn')){
+           console.log('Pushed delete button');
+           const itemKey = event.target.parentElement.dataset.key;
+           console.log(itemKey);
+           deleteBook(itemKey); 
+        }
+
+    });
 
     const node = document.createElement('li');
-    
-    node.textContent = `${book.title}`;
 
-    list.append(node);
+    node.setAttribute('data-key', book.id);
+    
+    node.textContent = `${book.title} by ${book.author}`;
+
+    node.insertAdjacentHTML("beforeend", "<button type=button class='del-btn'>X</button>");
+
+    if (item) {
+        list.replaceChild(node, item);
+    } else {
+        list.append(node);
+    }
 }
 
-function addBook(title) {
+function addBook(title, author) {
     const newBook = {
         title,
-        // author,
-        // checked: false
-    }
+        author,
+        id: Date.now(),
+    };
     
     myLibrary.push(newBook);
     renderBookList(newBook);
+}
+
+function deleteBook(key) {
+    const index = myLibrary.findIndex(item => item.id === Number(key));
+    
+    const book = {
+        deleted: true,
+        ...myLibrary[index]
+    };
+    
+    myLibrary = myLibrary.filter(item => item.id !== Number(key));
+    renderBookList(book);
 }
 
 const form = document.querySelector('#library-form');
 
 form.addEventListener('submit', event => {
     event.preventDefault();
-    const input = document.querySelector('#book-title');
+    const titleInput = document.querySelector('#book-title');
+    const authorInput = document.querySelector('#book-author');
 
-    const text = input.value.trim();
-    if (text !== '') {
-        addBook(text);
-        input.value = '';
-        input.focus();
+    const titleText = titleInput.value.trim();
+    const authorText = authorInput.value.trim();
+
+    if (titleText !== '' && authorText !== '') {
+        addBook(titleText, authorText);
+        titleInput.value = '';
+        authorInput.value = '';
+        titleInput.focus();
+        authorInput.focus();
     }
 
 });
-
-// function displayLibrary(array) {
-//     const bookDisplay = document.querySelector('#book-display');
-
-//     myLibrary.forEach((book) => {
-//         const item = document.createElement('p');
-//         item.textContent = `${book.title} by ${book.author}`;
-//         bookDisplay.appendChild(item);
-//     });
-// }
 
 const showFormBtn = document.querySelector('#show-form-btn');
 
@@ -67,41 +85,3 @@ showFormBtn.addEventListener('click', () => {
     formSection.classList.toggle('visible');
     showFormBtn.classList.toggle('visible');
 });
-
-// const addBookBtn = document.querySelector('#add-book');
-// const formElm = document.querySelector('#library-form');
-
-// addBookBtn.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     new FormData(formElm);
-// });
-
-// formElm.addEventListener('formdata', (e) => {
-//     console.log('formdata fired');
-
-//     let display = [];
-//     const data = e.formData;
-//     for (const value of data.values()) {
-//         display.push(value);
-//     }
-
-//     console.log("What's put in myLibrary");
-//     console.log(display);
-//     addBookToLibrary(display);
-// });
-
-// function addBookToLibrary(array) {
-//     let bookTitle = array[0];
-//     let bookAuthor = array[1];
-
-//     let book = new Book(bookTitle, bookAuthor);
-    
-//     myLibrary.push(book);
-
-//     console.log("Showing myLibrary:");
-//     console.table(myLibrary);
-// }
-
-// console.log('What myLibrary looks like at the start');
-// console.table(myLibrary);
-// displayLibrary();
